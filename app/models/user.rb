43c1eb2af8976f73be27ebd58deb64578f5d2a51
pvhiep <pvhiep96@gmail.com>
2019-10-03 16:50:27 +0700

    remove friend's assosiation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,10 @@
 class User < ApplicationRecord
   has_many :user_rooms, dependent: :destroy
   has_many :rooms, through: :user_rooms
-  has_many :friend_requests, dependent: :destroy
-  has_many :pending_friends, through: :friend_requests, source: :friend
-  has_many :friendships, dependent: :destroy
-  has_many :active_friends, through: :friendships, source: :friend
-  has_many :passive_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
-  has_many :passive_friends, through: :passive_friendships, source: 'user'
   has_many :messages
   has_many :permissions
 
   validates :email, uniqueness: true
   validates :name, presence: true
 
-  def friends
-    User.where("id = ? OR id = ?", active_friends.pluck(:friend_id), passive_friends.pluck(:user_id))
-  end
 end
