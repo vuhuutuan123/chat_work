b8c722dc4739528708a8ce07e52144a5b4bd3f7d
thaind <dinhthai2206@gmail.com>
2019-10-04 09:22:03 +0700

    fix cors methods

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module ChatWork
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*', headers: :any, methods: %i(get post delete put patch)
       end
     end
   end
